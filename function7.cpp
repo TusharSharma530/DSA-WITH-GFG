92d4affcc3b09180645a1ddf800926f3308c15f1
TusharSharma530 <tusharsharma6969@gmail.com>
2024-11-22 11:09:24 +0530

    added binary to decimal program

--- function7.cpp
+++ function7.cpp
@@ -17,10 +17,38 @@ int decimalToBinaryMethod(int n)
     return binaryno;
 }
 
+int decimalToBinaryMethod2(int n)
+{
+    int binaryno = 0;
+    int i = 0;
+
+    // bitwise method
+    while (n > 0)
+    {
+        int bit = (n & 1);
+        // cout << bit << endl;
+        binaryno = bit * pow(10, i++) + binaryno;
+        n = n >> 1;
+    }
+    return binaryno;
+}
+int binarytodecimal(int n)
+{
+    int decimal = 0;
+    int i = 0;
+    while (n)
+    {
+        int bit = n % 10;
+        decimal = decimal + bit * pow(2, i++);
+        n /= 10;
+    }
+    return decimal;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int binary = decimalToBinaryMethod(n);
-    cout << binary << endl;
+    //
+    int binaryno;
+    cin >> binaryno;
+    cout << binarytodecimal(binaryno) << endl;
 }
