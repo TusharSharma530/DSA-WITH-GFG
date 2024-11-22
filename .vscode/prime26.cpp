5c60e777a5405a923b5a92da1a82b5ea047b7ead
TusharSharma530 <tusharsharma6969@gmail.com>
2024-11-22 11:33:18 +0530

    printing prime number

---
+++ .vscode/prime26.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool checkprime(int n)
+{
+    int i = 2;
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        bool isprime = checkprime(i);
+        if (isprime)
+        {
+            cout << i << " ";
+        }
+    }
+}
